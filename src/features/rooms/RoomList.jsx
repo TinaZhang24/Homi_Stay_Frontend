dc6d85d53deadbfc0969f774d22ba00ccfda78cf
Qianni Zhang <zhangqianni24@gmail.com>
2024-10-31 02:37:12 +0800

    Added basic css styling for RoomList and added buttons for See Details and See Reviews.

--- src/features/rooms/RoomList.jsx
+++ src/features/rooms/RoomList.jsx
@@ -2,27 +2,29 @@ import { Link } from "react-router-dom";
 
 import { useGetRoomsQuery } from "./roomSlice";
 
-// import "./rooms.css";
+import "./rooms.css";
 
 export default function RoomList() {
   const { data: rooms = [] } = useGetRoomsQuery();
 
   return (
     <>
-      <div className="ListofRooms">
-        <ul>
-          {rooms.map((r) => (
-            <li key={r.id}>
-              <figure>
-                <img src={r.image} alt={r.roomName} />
-              </figure>
-              <h3>{r.roomName}</h3>
-              <h3>{r.type}</h3>
-              {/* <Link to={`/rooms/${r.id}`}>See details</Link> */}
-            </li>
-          ))}
-        </ul>
-      </div>
+      {/* <div className="ListofRooms"> */}
+      <ul className="ListofRooms">
+        {rooms.map((r) => (
+          <li key={r.id}>
+            <figure>
+              <img src={r.image} alt={r.roomName} />
+            </figure>
+            <h3>{r.roomName}</h3>
+            <h3>{r.type}</h3>
+            <button>See Details</button>
+            <button>See Reviews</button>
+            {/* <Link to={`/rooms/${r.id}`}>See details</Link> */}
+          </li>
+        ))}
+      </ul>
+      {/* </div> */}
     </>
   );
 }
